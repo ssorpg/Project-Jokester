010a36181bcf0a22f684386f09e23d6a8bfc4af6
Jonathan Baker <ssorpg@gmail.com>
2019-07-02 10:25:46 -0700

    more persistent storage and polishing

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -21,19 +21,6 @@ const relatedWordsAPI = 'https://api.datamuse.com/words?ml='; // URL to get our 
 const proxyURL = 'https://cors-anywhere.herokuapp.com/'; // CORS proxy
 
 // Dynamic
-var APIs = {
-    giphyMemes: { url: 'http://api.giphy.com/v1/gifs/search?limit=10&api_key=3dEcVRH1SquXQ50csTRKQnxK8aTT0yxt&q=', name: 'Giphy Memes', selected: true, searchable: true },
-    chuckNorrisJokes: { url: 'https://api.chucknorris.io/jokes/search?query=', name: 'Chuck Norris Jokes', selected: true, searchable: true },
-    generalJokes: { url: 'https://sv443.net/jokeapi/category/Any', name: 'General Jokes', selected: true, searchable: false },
-    geekJokes: { url: 'https://geek-jokes.sameerkumar.website/api', name: 'Geek Memes', selected: true, searchable: false },
-    corporateBS: { url: 'https://corporatebs-generator.sameerkumar.website', name: 'Corporate BS', selected: true, searchable: false },
-    ronSwansonQuotes: { url: 'https://ron-swanson-quotes.herokuapp.com/v2/quotes', name: 'Ron Swanson Quotes', selected: true, searchable: false },
-    yoMommaJokes: { url: 'https://api.yomomma.info', name: 'Yo Momma Jokes', selected: true, searchable: false },
-    dadJokes: { url: 'https://icanhazdadjoke.com/search?limit=10&term=', name: 'Dad Jokes', selected: true, searchable: true },
-    tronaldDumpQuotes: { url: 'https://api.tronalddump.io/search/quote?query=', name: 'Tronald Dump Quotes', selected: true, searchable: true },
-    xkcdComics: { url: 'https://relevant-xkcd-backend.herokuapp.com/search', name: 'XKCD Comics', selected: true, searchable: false }
-};
-
 var selectedUsername; // Username of current user
 
 
@@ -184,6 +171,8 @@ function getSuggestions(searchTerm) {
 }
 
 function getUser() {
+    localStorage.setItem('jokesterName', selectedUsername);
+
     registry.child(selectedUsername).once('value', snapshot => {
         if (snapshot.exists()) {
             snapshot = snapshot.val();
@@ -224,6 +213,12 @@ function pageSetup(snapshot) {
 
 // FUNCTION CALLS
 $(document).ready(function () { // Wait for page to load
+    if (localStorage.getItem('jokesterName')) {
+        selectedUsername = localStorage.getItem('jokesterName');
+
+        $('.usernameInput').val(selectedUsername);
+    }
+
     $('.login').on('click', event => {
         selectedUsername = $('.usernameInput').val();
 
@@ -243,7 +238,7 @@ $(document).ready(function () { // Wait for page to load
     $(document).on('click', '.singleJoke', event => {
         let target = $(event.target);
 
-        responsiveVoice.speak(target.text()); // Read out a clicked joke
+        responsiveVoice.speak(target.text(), "UK English Male", {pitch: 2}); // Read out a clicked joke
     });
 
     $(document).on('click', '.singleSuggestion', event => {
