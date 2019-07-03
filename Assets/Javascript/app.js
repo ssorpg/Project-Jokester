eb69ed49e971e254ce1afc2b2df507f477243e2b
Jonathan Baker <ssorpg@gmail.com>
2019-07-02 19:14:03 -0700

    commenting js, update readme and minor code changes

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -2,7 +2,7 @@
 // Static
 
 // Firebase
-const firebaseConfig = { // Use this for registering users and saving lastSearch + selectedAPIs
+const firebaseConfig = { // Use this for registering users and saving their search info
     apiKey: "AIzaSyCte7lhLlrSr3m3turhqLQQy3_3YSBymWE",
     authDomain: "trilogy-r-p-s.firebaseapp.com",
     databaseURL: "https://trilogy-r-p-s.firebaseio.com",
@@ -12,9 +12,9 @@ const firebaseConfig = { // Use this for registering users and saving lastSearch
     appId: "1:545722149214:web:633cee7329b7a2af"
 };
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig); // Initiate Firebase
 const database = firebase.database();
-const registry = database.ref('projectJokester/registry');
+const registry = database.ref('projectJokester/registry'); // Only need to use the registry currently
 
 // APIs
 const relatedWordsAPI = 'https://api.datamuse.com/words?ml='; // URL to get our suggestedTerms
@@ -28,7 +28,7 @@ var selectedUsername; // Username of current user
 // FUNCTIONS
 function getJokes(searchTerm) {
     $('.mainContent').empty(); // Clear content for next joke search
-    registry.child(selectedUsername).update({
+    registry.child(selectedUsername).update({ // Update the user's last search on Firebase
         searchTerm: $('.searchBar').val(),
         searchResults: ''
     })
@@ -40,20 +40,20 @@ function getJokes(searchTerm) {
             let searchHeaders = { // Create our search object for ajax
                 method: "GET",
                 headers: {
-                    'Accept': 'application/json'
+                    'Accept': 'application/json' // We only want JSON objects
                 }
             };
 
             if (APIs[api].searchable) {
                 apiURL += searchTerm; // For searchable APIs, add our searchTerm
             }
-            else if (api === 'xkcdComics') {
+            else if (api === 'xkcdComics') { // xkcdComics requires POST and uses PHP to search the JSON objects they store
                 let searchForm = $('<form>');
                 searchForm = searchForm.html('<input name="search" type="text" value="' + searchTerm + '">');
 
-                let searchData = new FormData(searchForm[0]); // Convert to regular JS element
+                let searchData = new FormData(searchForm[0]); // Convert to regular JS element and store in FormData
 
-                searchHeaders = {
+                searchHeaders = { // Sending off our FormData with our search query
                     method: "POST",
                     data: searchData,
                     contentType: false,
@@ -65,10 +65,11 @@ function getJokes(searchTerm) {
 
             $.ajax(searchHeaders).then(response => {
                 let currentJoke = formatJoke(response, api); // Get the actual joke from the response
-                let newJoke;
 
                 if (currentJoke) {
-                    if (currentJoke.type === 'string') {
+                    let newJoke; // Our joke element
+
+                    if (currentJoke.type === 'string') { // Strings can be read out loud and so have a 'Speak Joke' button added
                         newJoke = $('<p>').addClass('col-12 singleJoke ' + api).html('<button class="button"><span>🗣</span></button><span class="jokeText">' + currentJoke.joke + '</span>'); // Create the joke
                     }
                     else if (currentJoke.type === 'image') {
@@ -76,7 +77,7 @@ function getJokes(searchTerm) {
                     }
                     $('.mainContent').append(newJoke); // Put it on the page
 
-                    registry.child(selectedUsername).update({
+                    registry.child(selectedUsername).update({ // Update Firebase with the jokes we got
                         searchResults: $('.mainContent').html()
                     })
                 }
@@ -87,35 +88,35 @@ function getJokes(searchTerm) {
     getSuggestions(searchTerm); // Get our suggestedTerms
 }
 
-function formatJoke(response, api) { // Get jokes from response here
+function formatJoke(response, api) { // Parse joke response here
     let currentJoke = {};
 
-    try { // Catch arrays with no jokes in them (undefined)
+    try { // Catch no joke returned
         switch (api) {
             case 'chuckNorrisJokes':
-                currentJoke.joke = response.result[getRandomPos(response.result.length)].value; // Random joke
+                currentJoke.joke = response.result[getRandomPos(response.result.length)].value;
                 currentJoke.type = 'string';
                 break;
             case 'generalJokes':
                 if (response.type === 'twopart') {
-                    currentJoke.joke = response.setup + '<br><br>' + response.delivery; // Get two properties
+                    currentJoke.joke = response.setup + '<br><br>' + response.delivery; // Has two parts of the joke
                     currentJoke.type = 'string';
                 }
                 else if (response.type === 'single') {
-                    currentJoke.joke = response.joke; // String
+                    currentJoke.joke = response.joke;
                     currentJoke.type = 'string';
                 }
                 break;
             // case 'geekJokes':
-            //     currentJoke = response; // String
+            //     currentJoke = response;
             //     currentJoke.type = 'string';
             //     break;
             // case 'corporateBS':
-            //     currentJoke = response.phrase; // String
+            //     currentJoke = response.phrase;
             //     currentJoke.type = 'string';
             //     break;
             case 'ronSwansonQuotes':
-                currentJoke.joke = response[0]; // Array
+                currentJoke.joke = response[0];
                 currentJoke.type = 'string';
                 break;
             case 'yoMommaJokes':
@@ -123,12 +124,12 @@ function formatJoke(response, api) { // Get jokes from response here
                 currentJoke.type = 'string';
                 break;
             case 'dadJokes':
-                currentJoke.joke = response.results[getRandomPos(response.results.length)].joke; // Random joke
+                currentJoke.joke = response.results[getRandomPos(response.results.length)].joke;
                 currentJoke.type = 'string';
                 break;
             case 'giphyMemes':
-                currentJoke.joke = response.data[getRandomPos(response.data.length)].images.fixed_height.url; // Random image
-                currentJoke.joke = '<img src="' + currentJoke.joke + '">'; // Create image element
+                currentJoke.joke = response.data[getRandomPos(response.data.length)].images.fixed_height.url;
+                currentJoke.joke = '<img src="' + currentJoke.joke + '">'; // Create image html
                 currentJoke.type = 'image';
                 break;
             // case 'tronaldDumpQuotes':
@@ -136,20 +137,20 @@ function formatJoke(response, api) { // Get jokes from response here
             //     currentJoke.type = 'string';
             //     break;
             case 'xkcdComics':
-                currentJoke.joke = JSON.parse(response).results[0].image; // First image
-                currentJoke.joke = '<a href="' + currentJoke.joke + '" target="_blank"><img src="' + currentJoke.joke + '"></a>'; // Create image element
+                currentJoke.joke = JSON.parse(response).results[0].image;
+                currentJoke.joke = '<a href="' + currentJoke.joke + '" target="_blank"><img src="' + currentJoke.joke + '"></a>';
                 currentJoke.type = 'image';
         }
     }
     catch (error) {
-        console.log('ERR: No ' + api + ' found.');
+        console.log('ERR: No ' + api + ' found.'); // Sometimes we don't find any jokes in the search
     }
 
     return currentJoke;
 }
 
 function getRandomPos(length) {
-    let position = Math.floor(Math.random() * length); // Random number 1 to array length
+    let position = Math.floor(Math.random() * length); // Random number from 1 to array length
 
     return position;
 }
@@ -169,35 +170,35 @@ function getSuggestions(searchTerm) {
         while (numSuggestions < 10 && response[numSuggestions]) { // Only get up to 10 results
             let currentSuggestion = response[getRandomPos(response.length)].word;
 
-            let newSuggestion = $('<button>').addClass('col-6 singleSuggestion').html(currentSuggestion);
+            let newSuggestion = $('<button>').addClass('col-6 singleSuggestion').html(currentSuggestion); // New button element
             $('.suggestedContent').append(newSuggestion);
 
             numSuggestions++;
         }
 
-        registry.child(selectedUsername).update({
+        registry.child(selectedUsername).update({ // Update Firebase with our latest suggestedTerms
             suggestions: $('.suggestedContent').html()
         });
     });
 }
 
 function getUser() {
-    localStorage.setItem('jokesterName', selectedUsername);
+    localStorage.setItem('jokesterName', selectedUsername); // So user doesn't have to type in their name every time they visit (more advanced would be cookies)
 
-    registry.child(selectedUsername).once('value', snapshot => {
-        if (snapshot.exists()) {
+    registry.child(selectedUsername).once('value', snapshot => { // Let's see if the user's in the registry
+        if (snapshot.exists()) { // Found them
             snapshot = snapshot.val(); // Turn into a JS object for ease of use
 
             APIs = snapshot.APIs;
-            pageSetup(snapshot);
+            pageSetup(snapshot); // Put their info on the page
         }
-        else {
-            registry.child(selectedUsername).update({
+        else { // New user
+            registry.child(selectedUsername).update({ // Create them
                 name: selectedUsername,
                 APIs: APIs
             })
             
-            createAPIList();
+            createAPIList(); // Set their selected APIs
         }
     })
 }
@@ -212,7 +213,7 @@ function pageSetup(snapshot) {
 
 function createAPIList() {
     for (let api in APIs) {
-        let newLabel = $('<label>').addClass('selectAPI ' + api);
+        let newLabel = $('<label>').addClass('selectAPI ' + api); // Recreate the checkboxes and labels every time we load page
         newLabel.html('<input type="checkbox" apiID="' + api + '" checked>' + APIs[api].name);
 
         if (!APIs[api].selected) { // API not allowed
@@ -222,21 +223,21 @@ function createAPIList() {
         $('.checkBoxes').append(newLabel);
     }
 
-    $('.initial').css('display', 'none');
-    $('.wrapper').css('display', 'block');
+    $('.initial').css('display', 'none'); // Get rid of login page
+    $('.wrapper').css('display', 'block'); // Put up the search page!
 }
 
 
 
 // FUNCTION CALLS
 $(document).ready(function () { // Wait for page to load
-    if (localStorage.getItem('jokesterName')) {
-        selectedUsername = localStorage.getItem('jokesterName');
+    if (localStorage.getItem('jokesterName')) { // If they logged in before...
+        selectedUsername = localStorage.getItem('jokesterName'); // Get their login name
 
-        $('.usernameInput').val(selectedUsername);
+        $('.usernameInput').val(selectedUsername); // Put their name in the login field but don't log in (maybe they want to log in as someone else)
     }
 
-    $('.hamburgerButton').on('click', event => {
+    $('.hamburgerButton').on('click', event => { // Toggle for hamburger menu
         if ($('.checkBoxes').css('display') === 'none') {
             $('.checkBoxes').css('display', 'initial');
             $('.mainContent').css('display', 'none');
@@ -245,33 +246,34 @@ $(document).ready(function () { // Wait for page to load
             $('.checkBoxes').css('display', 'none');
             $('.mainContent').css('display', 'initial');
         }
+        // TODO: solve bug where clicking hamburger icon and resizing page won't reveal previously hidden elements because inline styling overwrites CSS
     })
 
-    $('.login').on('click', event => {
-        selectedUsername = $('.usernameInput').val();
+    $('.login').on('click', event => { // When clicking login...
+        selectedUsername = $('.usernameInput').val(); // Get their entered username
 
-        getUser(selectedUsername);
+        getUser(selectedUsername); // Log them in
     })
 
-    $('.usernameInput').on('keypress', event => {
-        if (event.keyCode === 13) { // 13 is the enter key
+    $('.usernameInput').on('keypress', event => { // When pressing a key...
+        if (event.keyCode === 13) { // If it's the enter key...
             event.preventDefault();
 
             selectedUsername = $('.usernameInput').val();
 
-            getUser(selectedUsername);
+            getUser(selectedUsername); // Log them in
         }
     });
 
-    $(document).on('click', '.button', event => {
+    $(document).on('click', '.button', event => { // 'Speak Joke' button
         if(responsiveVoice.isPlaying()) { // Button can be used as a toggle
             responsiveVoice.cancel();
             return;
         }
         
-        let target = $(event.target).parent().parent().find('.jokeText'); // I don't like this line of code but idk how to fix it
+        let target = $(event.target).parent().parent().find('.jokeText'); // I don't like this line of code but too lazy to make it better
 
-        responsiveVoice.speak(target.text(), "UK English Male", {pitch: 2}); // Read out a clicked joke
+        responsiveVoice.speak(target.text(), "UK English Male", {pitch: 2}); // Read out a clicked joke (in a funny voice)
     });
 
     $(document).on('click', '.singleSuggestion', event => {
@@ -293,7 +295,7 @@ $(document).ready(function () { // Wait for page to load
         if (event.keyCode === 13) { // 13 is the enter key
             event.preventDefault();
 
-            let searchTerm = $('.searchBar').val(); // Get search term from search bar
+            let searchTerm = $('.searchBar').val();
 
             getJokes(searchTerm);
         }
@@ -301,7 +303,7 @@ $(document).ready(function () { // Wait for page to load
 
     $('.suggestButton').on('click', event => {
         let searchTerm = randWords[getRandomPos(randWords.length)].word; // Get a random word from our dictionary
-        $('.searchBar').val(searchTerm);
+        $('.searchBar').val(searchTerm); // Put the word in the search bar as if it had been typed
 
         getJokes(searchTerm);
     });
@@ -326,7 +328,7 @@ $(document).ready(function () { // Wait for page to load
             }
         }
 
-        registry.child(selectedUsername).update({
+        registry.child(selectedUsername).update({ // Update Firebase with the selected APIs
             APIs: APIs
         })
     });
